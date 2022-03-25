1dffa80b2a694625c997b3e83a1acb8d204566e6
APiccolo89 <piccolo@uni-mainz.de>
2022-03-25 14:50:19 +0100

    As part of Julia Hackaton, i would like part of my script in julia

---
+++ Slab_Detachment.jl
@@ -0,0 +1,16 @@
+"""
+Project Julia Hackaton: convert my python script into julia
+1) Produce fast plotting routine
+2) Produce Data Base slab detachment
+3) Parallel writing of database
+-> Aims:
+I want to write a script capable to post processes
+several test at the same time, and update a Data
+Base that contains the relevant data for my  project
+to later use.
+?) Object oriented
+    -> How to produce classes of data that perform
+    basic function?
+    ->How to produce plot?
+    ->How to optimize?
+"""
